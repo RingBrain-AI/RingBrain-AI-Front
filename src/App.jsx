b0c904a6479f75e6deb2614a628f050c4a35d8cb
jorge henriquez <jorgeahenriqueznovoa@gmail.com>
2025-11-27 17:18:22 -0500

    chore: initialize React project with Vite and basic setup

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div className="min-h-screen flex items-center justify-center bg-gray-100">
+        <h1 className="text-4xl font-bold text-blue-600">Tailwind is working</h1>
+        <div className="w-48 h-12 bg-red-500 mt-6"></div>
+      </div>
+    </>
+  )
+}
+
+export default App
